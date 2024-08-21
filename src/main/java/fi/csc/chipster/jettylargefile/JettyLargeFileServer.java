ffcf45bb158a511fae924163e6b95b77e288a437
Petri Klemela <petri.klemela@csc.fi>
2024-08-21 11:41:00 +0300

    Test different sizes

--- src/main/java/fi/csc/chipster/jettylargefile/JettyLargeFileServer.java
+++ src/main/java/fi/csc/chipster/jettylargefile/JettyLargeFileServer.java
@@ -1,26 +1,32 @@
 package fi.csc.chipster.jettylargefile;
 
+import java.io.IOException;
+import java.net.StandardSocketOptions;
 import java.net.URI;
+import java.nio.channels.SocketChannel;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.io.Connection;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import jdk.net.ExtendedSocketOptions;
 
 public class JettyLargeFileServer {
 
     private Server httpServer;
 
-    public void startServer() throws Exception {
+    public void startServer(int port, boolean tcpKeepalive) throws Exception {
 
         Logger logger = LogManager.getLogger();
 
         ServletContextHandler servletHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         servletHandler.setContextPath("/");
 
-        URI baseUri = URI.create("http://0.0.0.0:8800");
+        URI baseUri = new URI("http", null, "0.0.0.0", port, null,
+                null, null);
 
         servletHandler.addServlet(
                 new ServletHolder(new JettyLargeFileServlet()),
@@ -31,6 +37,31 @@ public class JettyLargeFileServer {
         ServerConnector connector = new ServerConnector(httpServer);
         connector.setPort(baseUri.getPort());
         connector.setHost(baseUri.getHost());
+
+        if (tcpKeepalive) {
+
+            logger.info("configure tcp keepalive");
+
+            Connection.Listener listener = new Connection.Listener() {
+                @Override
+                public void onOpened(Connection connection) {
+                    Object t = connection.getEndPoint().getTransport();
+                    if (t instanceof SocketChannel c) {
+                        try {
+                            c.setOption(StandardSocketOptions.SO_KEEPALIVE, true);
+                            c.setOption(ExtendedSocketOptions.TCP_KEEPIDLE, 2);
+                            // c.setOption(ExtendedSocketOptions.TCP_KEEPCOUNT, 2);
+                            c.setOption(ExtendedSocketOptions.TCP_KEEPINTERVAL, 1);
+                        } catch (IOException e) {
+                            logger.warn("failed to set socket options", e);
+                        }
+
+                    }
+                }
+            };
+            connector.addBean(listener);
+        }
+
         httpServer.addConnector(connector);
 
         httpServer.setHandler(servletHandler);
@@ -38,12 +69,11 @@ public class JettyLargeFileServer {
         httpServer.start();
 
         logger.info("listening " + baseUri);
-
-        httpServer.join();
     }
 
     public static void main(String[] args) throws Exception {
         JettyLargeFileServer jlf = new JettyLargeFileServer();
-        jlf.startServer();
+        jlf.startServer(8800, false);
+        jlf.startServer(8801, true);
     }
 }
--- src/main/java/fi/csc/chipster/jettylargefile/JettyLargeFileServlet.java
+++ src/main/java/fi/csc/chipster/jettylargefile/JettyLargeFileServlet.java
@@ -3,10 +3,11 @@ package fi.csc.chipster.jettylargefile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.NullInputStream;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,12 +15,39 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JettyLargeFileServlet extends HttpServlet {
 
+    Logger logger = LogManager.getLogger();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        String[] defaultSize = { "1Gi" };
+
         long GiB = 1024l * 1024 * 1024;
 
-        InputStream fileStream = new NullInputStream(150 * GiB);
+        String sizeString = request.getParameterMap().getOrDefault("size", defaultSize)[0];
+
+        logger.info("requested size: " + sizeString);
+
+        long scale = 1;
+
+        if (sizeString.contains("ki")) {
+            sizeString = sizeString.replace("ki", "");
+            scale = 1024l;
+        }
+
+        if (sizeString.contains("Mi")) {
+            sizeString = sizeString.replace("Mi", "");
+            scale = 1024l * 1024;
+        }
+
+        if (sizeString.contains("Gi")) {
+            sizeString = sizeString.replace("Gi", "");
+            scale = 1024l * 1024 * 1024;
+        }
+
+        long size = Long.parseLong(sizeString) * scale;
+
+        InputStream fileStream = new NullInputStream(size);
 
         response.setStatus(HttpServletResponse.SC_OK);
 
